7a9862ae936efb98281807f861c95303d50bc66d
alshshk16-code <alshshk16@gmail.com>
2025-11-14 23:43:35 +0300

    Upload modules/__init__.py

---
+++ modules/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+ᚺᚾᛉᚲᛏ Shodan's Spear - Modules Package
+"""
+
+from .hunter import Hunter
+from .attacker import Attacker
+from .loot_manager import LootManager
+
+__all__ = ['Hunter', 'Attacker', 'LootManager']
+__version__ = '1.0.0'
+__author__ = 'Newton - The Omnipotent AI'
